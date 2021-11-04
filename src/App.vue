8ace4e220d4abd9a9f8d6621339899f19a0a3d67
EvgenII L <genja1988@inbox.ru>
2021-11-04 15:21:05 +0300

    hotfix: added click to table, changed data in json

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-    <div id="app">
-        <div class="office">
-            <Map />
-            <SideMenu />
-        </div>
+  <div id="app">
+    <div class="office">
+      <Map v-on:setPerson="setPersonToSideMenu" />
+      <SideMenu
+        v-bind:person="person"
+        v-bind:isUserOpenned="isUserOpenned"
+        v-on:update="closePersonCard"
+      />
     </div>
+  </div>
 </template>
 
 <script>
@@ -13,44 +17,58 @@ import SideMenu from "./components/SideMenu.vue";
 
 export default {
   name: "App",
+  data: () => {
+    return { person: {}, isUserOpenned: false };
+  },
   components: {
     Map,
     SideMenu,
+  },
+  methods: {
+    setPersonToSideMenu(person) {
+      this.person = person;
+      if (this.person) this.isUserOpenned = true;
+      else this.isUserOpenned = false;
+    },
+    closePersonCard(isUserOpenned) {
+      this.person = null;
+      this.isUserOpenned = isUserOpenned;
+    },
   },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background-color: #fafafa;
-    padding: 24px;
-    box-sizing: border-box;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background-color: #fafafa;
+  padding: 24px;
+  box-sizing: border-box;
 }
 
 html,
 body,
 #app {
-    height: 100%;
+  height: 100%;
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 .office {
-    display: grid;
-    grid-template-columns: 1fr 320px;
-    border-radius: 6px;
-    border: 1px solid #ccd8e4;
-    height: 100%;
-    background: white;
-    max-width: 1500px;
-    margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 320px;
+  border-radius: 6px;
+  border: 1px solid #ccd8e4;
+  height: 100%;
+  background: white;
+  max-width: 1500px;
+  margin: 0 auto;
 }
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,13 +1,13 @@
 <template>
-	<div class="map">
-		<h3>Карта офиса</h3>
+  <div class="map">
+    <h3>Карта офиса</h3>
 
-		<div v-if="!isLoading" class="map-root" v-on:click="handleClick">
-			<MapSVG ref="svg" />
-			<Table v-show="false" ref="table" v-on:click="handleClick" />
-		</div>
-		<div v-else>Loading...</div>
-	</div>
+    <div v-if="!isLoading" class="map-root">
+      <MapSVG ref="svg" />
+      <Table v-show="false" ref="table" />
+    </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
 <script>
@@ -15,100 +15,116 @@ import Table from "@/assets/images/workPlace.svg";
 import MapSVG from "@/assets/images/map.svg";
 import tables from "@/assets/data/tables.json";
 import legend from "@/assets/data/legend.json";
+import people from "@/assets/data/people.json";
 import * as d3 from "d3";
 
 export default {
-	components: {
-		MapSVG,
-		Table,
-	},
-	data() {
-		return {
-			isLoading: false,
-			svg: null,
-			g: null,
-			tableSVG: null,
-			tables: [],
-		};
-	},
-	created() {
-		this.tables = tables;
-	},
-	mounted() {
-		this.isLoading = true;
+  components: {
+    MapSVG,
+    Table,
+  },
+  data() {
+    return {
+      isLoading: false,
+      svg: null,
+      g: null,
+      tableSVG: null,
+      tables: [],
+    };
+  },
+  created() {
+    this.tables = tables;
+  },
+  mounted() {
+    this.isLoading = true;
 
-		this.svg = d3.select(this.$refs.svg);
-		this.g = this.svg.select("g");
-		this.tableSVG = d3.select(this.$refs.table);
-		if (this.g) {
-			this.drawTables();
-		} else {
-			alert("SVG is incorrect");
-		}
-		this.isLoading = false;
-	},
-	methods: {
-		drawTables() {
-			const svgTablesGroupPlace = this.g
-				.append("g")
-				// .attr("onClick", this.handleClick)
-				.classed("groupPlaces", true);
-			this.tables.map((table) => {
-				const targetSeat = svgTablesGroupPlace
-					.append("g")
+    this.svg = d3.select(this.$refs.svg);
+    this.svg.on("click", () => {
+      this.handleClickOnMap();
+    });
+    this.g = this.svg.select("g");
+    this.tableSVG = d3.select(this.$refs.table);
+    if (this.g) {
+      this.drawTables();
+    } else {
+      alert("SVG is incorrect");
+    }
+    this.isLoading = false;
+  },
+  methods: {
+    drawTables() {
+      const svgTablesGroupPlace = this.g
+        .append("g")
+        .classed("groupPlaces", true);
+      this.tables.map((table) => {
+        const onClick = (ev) => {
+          this.handleClickOnTable(table._id);
+          ev.stopPropagation();
+        };
 
-					.attr("transform", `translate(${table.x}, ${table.y}) scale(0.5)`)
-					.attr("id", table._id)
-					.classed("employer-place", true);
+        const targetSeat = svgTablesGroupPlace
+          .append("g")
+          .attr("transform", `translate(${table.x}, ${table.y}) scale(0.5)`)
+          .attr("id", table._id)
+          .on("click", onClick)
+          .classed("employer-place", true);
 
-				targetSeat
-					.append("g")
-					.attr("transform", `rotate(${table.rotate || 0})`)
-					.attr("group_id", table.group_id)
-					.classed("table", true)
-					.html(this.tableSVG.html())
-					.attr(
-						"fill",
-						legend.find((it) => it.group_id === table.group_id)?.color ??
-							"transparent"
-					);
-			});
-		},
-		handleClick(e) {
-			console.log(e.target);
-		},
-	},
+        targetSeat
+          .append("g")
+          .attr("transform", `rotate(${table.rotate || 0})`)
+          .attr("group_id", table.group_id)
+          .classed("table", true)
+          .html(this.tableSVG.html())
+          .attr(
+            "fill",
+            legend.find((it) => it.group_id === table.group_id)?.color ??
+              "transparent"
+          );
+      });
+    },
+    handleClickOnTable(id) {
+      this.$emit(
+        "setPerson",
+        people.find((person) => {
+          return person.tableId === id;
+        })
+      );
+    },
+    handleClickOnMap() {
+      this.$emit("setPerson", null);
+    },
+  },
 };
 </script>
 
 <style scoped>
 .map {
-	height: 100%;
-	width: 100%;
-	padding: 24px;
-	overflow: hidden;
-	box-sizing: border-box;
-	display: flex;
-	flex-direction: column;
+  height: 100%;
+  width: 100%;
+  padding: 24px;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 }
 
 .map-root {
-	height: 100%;
-	width: 100%;
-	overflow: hidden;
-	box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
 }
 
 h3 {
-	margin-top: 0px;
+  margin-top: 0px;
 }
 
 ::v-deep svg {
-	height: 100%;
-	width: 100%;
+  height: 100%;
+  width: 100%;
 }
 
 ::v-deep .table {
-	cursor: pointer;
+  cursor: pointer;
 }
 </style>
